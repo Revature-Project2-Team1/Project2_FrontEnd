bf6e9ff8436c42ce62eb65a9a4da34594df86ea7
Phuc Huynh <43251722+Phuc15@users.noreply.github.com>
2021-05-23 08:46:25 -0700

    angular setup

--- Project2_VaccineTrackingSystem_FrontEnd/Vaccine-Tracker/src/app/app.module.ts
+++ Project2_VaccineTrackingSystem_FrontEnd/Vaccine-Tracker/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsumerLoginComponent } from './consumer-login/consumer-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsumerLoginComponent
   ],
   imports: [
     BrowserModule,
