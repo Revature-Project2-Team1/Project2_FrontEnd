54cf60fa1feddd714242f9b3f1d9287f347f3325
Miloincome <63473252+Miloincome@users.noreply.github.com>
2021-05-24 22:27:07 -0500

    Vaxify Angular Single Page App Login+Home Component Added

---
+++ Vaxify2/src/app/login-patient/login-patient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './login-patient.component.html',
+  styleUrls: ['./login-patient.component.css']
+})
+export class LoginPatientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
