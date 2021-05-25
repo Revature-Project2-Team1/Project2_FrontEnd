c07c7111ed1a450bee5b4d56704121c975cc9e9e
Anthony-Tag <anthony.escalante@revature.net>
2021-05-25 17:06:11 -0400

    provider login

--- Vaxify2/src/app/app-routing.module.ts
+++ Vaxify2/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {LoginPatientComponent} from './login-patient/login-patient.component';
+import { LoginProviderComponent } from './login-provider/login-provider.component';
 
 const routes: Routes = [
 {path: 'loginPatient', component:LoginPatientComponent},
-{path: 'home', component:HomeComponent}
+{path: 'home', component:HomeComponent},
+{path: 'loginProvider', component:LoginProviderComponent}
 
 
 ];
