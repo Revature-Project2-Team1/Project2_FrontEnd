ead3c6d05a9d6a485a405fd94f9dc9afb7f41f61
Anthony-Tag <anthony.escalante@revature.net>
2021-05-28 01:39:51 -0400

    provider error added not working

--- Vaxify2/src/app/services/RegisterService/register.service.ts
+++ Vaxify2/src/app/services/RegisterService/register.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-import {fromEvent} from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { environment } from '../../../environments/environment';
+import { PatientCreds } from '../../models/patient-creds';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
-  
+  constructor(private router: Router, private http: HttpClient) {}
+
+  register(patientCreds: PatientCreds) {
+    return this.http.post(`${environment.apiUrl}/patient/patientCreds`,patientCreds);//insert post method here
+}
 }
