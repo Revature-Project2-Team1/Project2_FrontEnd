6a4c0cff6aa1ed24185f30ee71abb507bfa0491b
Abeil Adilow <abeil.adilow@revature.net>
2021-05-29 14:47:26 -0400

    removed circle timer

--- Vaxify2/src/app/qr-code-generator-reader/qr-code-generator-reader.component.ts
+++ Vaxify2/src/app/qr-code-generator-reader/qr-code-generator-reader.component.ts
@@ -10,11 +10,17 @@ import { Router } from '@angular/router';
 export class QrCodeGeneratorReaderComponent implements OnInit {
   elementType: NgxQrcodeElementTypes;
   correctionLevel: NgxQrcodeErrorCorrectionLevels;
-  value: string;
   percent:number;
   percent_e: number;
+  duration:number;
+  
+  value="";
+  animation=true;
+  color=""
+  status=false;
+  inboundClick=true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -22,54 +28,29 @@ export class QrCodeGeneratorReaderComponent implements OnInit {
   verifyQR(): void{
 
   }
+  uploadFile($event) {
+    console.log($event.target.files[0]); // outputs the first file
+}
 
   generateQR(): void{
 
     
     this.inboundClick = false;
 
+    this.animation=!this.animation;
+   
     this.elementType = NgxQrcodeElementTypes.URL;
     this.correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
-    this.value="THIS IS WHAT WILL BE IN THE QR CODE. IDEALLY JSON. PLAY AROUND WITH THIS TO SEE QR CHANGE";
-    this.percent=100;
-    this.percent_e=1;
-
-    function sayHi() {
-      this.percent_e=100;
-    }
-    function reset(){
-
-    }
-    setTimeout(sayHi, 6000);
-
+    this.value="THIS IS WHAT WILL BE IN THE QR CODE. IDEALLY JSON. PLAY AROUND WITH THIS TO SEE QR CHANGE"+this.animation;
     
-  }
-
-  formatTitle = (percent) : string => {
-    if(percent >= 100){
-      return "EXPIRED"
-    }else if(percent > 0){
-      return "Valid"
-    }else {
-      return "N/A"
+    function runC() {
+    
+      
     }
-  }
-
-  formatColor = (status: string) : string => {
-    if(status=="EXPIRED"){
-      return "#FF0000"
-    }else if(status=="Valid"){
-      return "#FFFFFF"
-    }else {
-      return "#FFFFFF"
-    }
-  }
-
-  duration:number=6000;
   
+    setTimeout(runC, 6000); 
+  }
 
-
-  inboundClick = true;
 
 
   
