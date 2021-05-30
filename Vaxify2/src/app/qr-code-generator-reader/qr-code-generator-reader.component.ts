ccfa24fcc156429a54866c3741dcb45df65df506
Abeil Adilow <abeil.adilow@revature.net>
2021-05-30 03:16:03 -0400

    qr mvp done

--- Vaxify2/src/app/qr-code-generator-reader/qr-code-generator-reader.component.ts
+++ Vaxify2/src/app/qr-code-generator-reader/qr-code-generator-reader.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Patient } from '../models/patient';
 import { PatientService } from '../services/PatientService/patient.service';
 import jsQR from "jsqr";
+import QrcodeDecoder from 'qrcode-decoder';
 
 
 @Component({
@@ -19,6 +20,7 @@ export class QrCodeGeneratorReaderComponent implements OnInit {
   duration:number;
   
   value="";
+  upload_value:string;
   animation=true;
   color=""
   status=false;
@@ -29,10 +31,16 @@ export class QrCodeGeneratorReaderComponent implements OnInit {
   pass:HTMLAudioElement= new Audio("../../assets/pass.mp3");
   fail:HTMLAudioElement=new Audio("../../assets/fail.mp3");
   
- 
+  imageData:ImageData;
+  code:any;
+  
+  reader = new FileReader();
+  pic= new Image();
+  qr= new QrcodeDecoder();
 
  private patient: Patient;
   patient_status: string;
+  val: string | ArrayBuffer;
   constructor(private service: PatientService) { 
 
   }
@@ -40,46 +48,69 @@ export class QrCodeGeneratorReaderComponent implements OnInit {
 
 
   verifyQR(): void{
+    
+
+     this.code = jsQR(this.imageData.data, 250,250);
 
   }
-  uploadFile($event) {
-    console.log($event.target.files[0]); // outputs the first file
+  
+  uploadFile($event) { //also decodes QR
+    this.reader.readAsDataURL($event.target.files[0]);
+    this.reader.onload = (_event) => {  
+      this.qr.decodeFromImage(this.reader.result)
+      .then(res=> {
+        console.log(res.data)
+        this.upload_value=res.data
+        if(this.upload_value=="Vaccinated"){
+            this.pass.load();
+            this.pass.volume=1;
+            this.pass.play();   
+            alert("Vaccinated!")
+        }
+        else{
+          this.fail.load();
+          this.fail.volume=1;
+          this.fail.play();   
+          alert("Not Vaccinated!")
+        }
+      });
+        
+    } 
+
 }
 
   generateQR(): void{
     this.inboundClick = false;
     
+
     
+    this.fail.load();
+    this.fail.volume=1;
+    this.fail.play();   
+    
+    
+   
+  }
+
+  ngOnInit(): void {
+    this.elementType = NgxQrcodeElementTypes.URL;
+    this.correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+
     this.service.generateQR(this.current_user).subscribe(res => {
       this.patient=res;
       console.log(this.patient);
       console.log("test")
       this.patient_status=this.patient.status;
+      this.value=this.patient_status;
 
 
   })
 
-    
-  this.fail.load();
-  this.fail.volume=1;
-    this.fail.play();
-    this.animation=!this.animation;
-   
-    this.elementType = NgxQrcodeElementTypes.URL;
-    this.correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
-    this.value=this.patient_status;
-    function runC() {
-    
-      
-    }
+  }
+
   
-    setTimeout(runC, 6000); 
-  }
-
-  ngOnInit(): void {
-   
-
-  }
 
   
 }
+
+
