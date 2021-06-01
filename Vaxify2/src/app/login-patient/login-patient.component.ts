2f62647ab75f4b3edd90c19ce0f3fb44820a5b93
Phuc Huynh <43251722+Phuc15@users.noreply.github.com>
2021-06-01 02:21:06 -0700

    billy

--- Vaxify2/src/app/login-patient/login-patient.component.ts
+++ Vaxify2/src/app/login-patient/login-patient.component.ts
@@ -64,7 +64,7 @@ export class LoginPatientComponent implements OnInit {
           this.patient = res;
           //console.log(this.patient != null);
           if (this.patient != null) {
-            sessionStorage.setItem('patient', this.patient.ssn);
+            sessionStorage.setItem('patient', this.patient.customerSSN);
             swal('Yay','You are in','success');
             this.router.navigate(['/patient-dashboard'], {
               relativeTo: this.route,
@@ -86,8 +86,8 @@ export class LoginPatientComponent implements OnInit {
             this.patient = res;
             //console.log(this.patient != null);
             if (this.patient != null) {
-              sessionStorage.setItem('patient', this.patient.ssn);
-              swal('Yay','You are in','success');
+              sessionStorage.setItem('patient', this.patient.customerSSN);
+              swal('Yay','you are in','success');
               this.router.navigate(['../patient-dashboard'], {
                 relativeTo: this.route,
               });
